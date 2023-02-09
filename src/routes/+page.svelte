015c38794083bf093050b92746439a2c831c1f0e
multimodalart <joaopaulo.passos+multimodal@gmail.com>
2023-02-09 15:00:11 +0100

    Dynamic loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,18 @@
 
 	let models = [];
 	let cursor = null;
+	let loading = false;
 
 	onMount(async () => {
 		await loadModels();
+		await getImageUrl();
 	});
 
 	async function loadModels() {
+		if (loading) return;
+		loading = true;
 		let url =
-			"https://huggingface.co/api/models?filter=diffusers&sort=likesRecent&direction=-1&full=1&limit=25";
+			"https://huggingface.co/api/models?filter=diffusers&sort=likesRecent&direction=-1&full=1&limit=24";
 		if (cursor) {
 			url += `&cursor=${cursor}`;
 		}
@@ -26,39 +30,72 @@
 			});
 		}
 		models = [...models, ...(await res.json())];
+		loading = false;
 	}
-	function getImageUrl(id, siblings) {
+	async function getImageUrl(id, siblings) {
+		//Tries to get the first image avaliable in the repo
 		let image = siblings.find((sibling) => {
 			let extension = sibling.rfilename.split(".").pop().toLowerCase();
 			return ["jpg", "jpeg", "png", "webp"].includes(extension);
 		});
-		console.log(image);
-		return image
-			? "https://huggingface.co/" +
-					id +
-					"/resolve/main/" +
-					image.rfilename
-			: "https://placehold.co/300x300";
+
+		const baseUrl = "https://huggingface.co/" + id;
+
+		if (image) {
+			return `${baseUrl}/resolve/main/${image.rfilename}`;
+		} else {
+			//If no image is avaliable in the repo, tries to get the first URL to an image avaliable in the README.md
+			const regex =
+				/\b(https?):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*\.(?:jpe?g|png|gif|bmp|webp)\b/gi;
+			const res = await fetch(`${baseUrl}/resolve/main/README.md`);
+			const readmeContent = await res.text();
+			const matches = readmeContent.match(regex);
+
+			return matches ? matches[0] : "https://placehold.co/300x300";
+		}
+	}
+	function handleScroll() {
+		const scrollTop =
+			window.pageYOffset ||
+			document.documentElement.scrollTop ||
+			document.body.scrollTop;
+		const scrollHeight = document.body.scrollHeight;
+		const clientHeight =
+			document.documentElement.clientHeight || window.innerHeight;
+		if (scrollTop + clientHeight >= scrollHeight - 1) {
+			loadModels();
+		}
 	}
 </script>
 
+<svelte:window on:scroll={handleScroll} />
 <h2>Most liked models last month 🤗</h2>
-<p>Diffusers models most liked on Hugging Face</p>
+<p>Diffusers models most liked on Hugging Face last month</p>
 <div class="grid">
 	{#each models as model}
 		<div class="grid-item">
-			<h3><a href="https://huggingface.co/{model.id}">{model.id}</a></h3>
-			<img
-				width="300"
-				src={getImageUrl(model.id, model.siblings)}
-				alt="example"
-			/>
+			<h3>
+				<a
+					href="https://huggingface.co/{model.id}"
+					target="_blank"
+					rel="noreferrer">{model.id}</a
+				>
+			</h3>
+			{#await getImageUrl(model.id, model.siblings)}
+				<p>loading...</p>
+			{:then model_image}
+				<img width="300" src={model_image} alt="example" />
+			{:catch error}
+				<img
+					width="300"
+					src="https://placehold.co/300x300"
+					alt="example"
+				/>
+			{/await}
 			<p>Likes: {model.likesRecent}</p>
 		</div>
 	{/each}
 </div>
-
-<button type="button" on:click={loadModels}>Load more</button>
 
 <style>
 	.grid {
